37fc8bc2183dbc02689dc09569e7d6af62479a1e
Erich(Renyong) Wang <eriwan@microsoft.com>
2020-06-05 00:05:35 +0800

    update runnable test modes for KeyVault tests

--- sdk/keyvault/Azure.Management.KeyVault/tests/VaultOperationsTestsBase.cs
+++ sdk/keyvault/Azure.Management.KeyVault/tests/VaultOperationsTestsBase.cs
@@ -14,7 +14,7 @@ using Azure.Management.Resources;
 namespace Azure.Management.KeyVault.Tests
 {
     [ClientTestFixture]
-    [RunOnlyInTestModes(Live = true)]
+    [RunOnlyInTestModes(RecordOrPlayback = true)]
     public abstract class VaultOperationsTestsBase : RecordedTestBase<KeyVaultManagementTestEnvironment>
     {
         private const string ObjectIdKey = "ObjectId";
